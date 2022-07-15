e1357d477e1d6d5c07d632e2871a70be210f303b
fcablik-maxis <cablik.filip@gmail.com>
2022-07-15 20:42:45 +0200

    main_js

--- src/App.js
+++ src/App.js
@@ -4,20 +4,20 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
 
 function App() {
-  return (
-    <>
-    <Router>
+    return (
+        <>
+            <Router>
 
-      <Header />
-      <Main />
+                <Header />
+                <Main />
 
-      <Routes>
-        <Route path='/' exact />
-      </Routes>
+                <Routes>
+                <Route path='/' exact />
+                </Routes>
 
-    </Router>
-    </>
-  )
+            </Router>
+        </>
+    )
 }
 
 export default App
