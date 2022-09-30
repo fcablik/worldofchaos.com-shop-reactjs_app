65800351a84e3c57005053f473310e2ba162327b
hussleverse <filip.cablik@icloud.com>
2022-09-30 21:52:35 +0200

    new woch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import React from 'react';
-// import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 import './App.css';
 
+// import Header from './Header';
+// import Footer from './Footer';
+// import Navigation from './Navigation';
+
+import { Link } from 'react-router-dom';
 import AppDev from './components/_dev/_App';
+import HomeDev from './components/pages/_dev/HomeDev';
 
 function App() {
     return (
         <>
-            <AppDev />
+
+            <Router>
+
+                <main id='main'>
+                    <div className='container'>
+
+                        <Routes>
+                            <Route exact path="/" element={<HomeDev />}/>
+                            <Route exact path="/dev/" page={<AppDev />}/>
+                        </Routes>
+
+                        <Link to="/" className='-cc-text'>
+                            <sup>00</sup>HOME
+                        </Link>
+                        <Link to="/dev/" className='-cc-text'>
+                            <sup>00</sup>TEXT HERE
+                        </Link>
+
+                    </div>
+                </main>
+
+            </Router>
+
         </>
     )
 }
